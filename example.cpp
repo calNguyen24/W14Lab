b6ee73df52fa00a9c763e0a728293518ab02c8f6
Calvin <cnguyen1394@student.cccd.edu>
2024-11-30 21:36:18 -0800

    Committing example.cpp for assignment 4 lab

---
+++ example.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int Fibonacci(int n) {
+   
+   if(n < 0){
+    return -1;
+   }
+   if(n <= 1){
+    return n;
+   }
+   return Fibonacci(n - 1) + Fibonacci(n - 2) ;
+      
+}
+
+int main() {
+   int startNum;
+   
+   cin >> startNum;  
+   cout << "Fibonacci(" << startNum << ") is " << Fibonacci(startNum) << endl;
+   
+   return 0;
+}
